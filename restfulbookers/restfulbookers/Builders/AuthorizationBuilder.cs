35033a7f35bb477f230dd864420974b36f4539fa
rokoszmaciej <40142376+rokoszmaciej@users.noreply.github.com>
2024-09-10 15:24:44 +0200

    add builder with required files

---
+++ restfulbookers/restfulbookers/Builders/AuthorizationBuilder.cs
@@ -0,0 +1,36 @@
+﻿using RestfulBookerTestFramework.Models.Auth.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulBookerTestFramework.Builders
+{
+    public class AuthorizationBuilder
+    {
+        private readonly AuthorizationModel _authorizationModel;
+
+        public AuthorizationBuilder() 
+        { 
+            _authorizationModel = new AuthorizationModel();
+        }
+
+        public AuthorizationBuilder WithUserName(string userName)
+        {
+            _authorizationModel.Username = userName;
+            return this;
+        }
+
+        public AuthorizationBuilder WithPassword(string password)
+        {
+            _authorizationModel.Password = password;
+            return this;
+        }
+
+        public AuthorizationModel Build()
+        {
+            return _authorizationModel;
+        }
+    }
+}
---
+++ restfulbookers/restfulbookers/Builders/RestRequestBuilder.cs
@@ -0,0 +1,49 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulBookerTestFramework.Builders
+{
+    public class RestRequestBuilder
+    {
+        private readonly RestRequest _restRequest;
+
+        public RestRequestBuilder()
+        {
+            _restRequest = new RestRequest();
+        }
+
+        public RestRequestBuilder WithResource(string resource)
+        {
+            _restRequest.Resource = resource;
+            return this;
+        }
+
+        public RestRequestBuilder WithMethod(Method method) 
+        {
+            _restRequest.Method = method;
+            return this;
+        }
+
+        public RestRequestBuilder WithHeader(string name, string value) 
+        {
+            _restRequest.AddHeader(name, value);
+            return this;
+        }
+
+        public RestRequestBuilder WithBody(object body)
+        {
+            _restRequest.AddJsonBody(body);
+            return this;
+        }
+
+        public RestRequest Build()
+        {
+            return _restRequest;
+        }
+
+    }
+}
