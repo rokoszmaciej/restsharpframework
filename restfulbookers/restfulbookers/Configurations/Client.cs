4ab40f82566487a7d247d21173e7feb253da5d3a
rokoszmaciej <40142376+rokoszmaciej@users.noreply.github.com>
2024-09-10 15:25:56 +0200

    add configuration content

---
+++ restfulbookers/restfulbookers/Configurations/Client.cs
@@ -0,0 +1,28 @@
+﻿using Allure.Net.Commons;
+using RestfulBookerTestFramework.Configurations;
+using RestfulBookerTestFramework.Utilities;
+using RestfulBookerTestFramework.Utilities.Allure;
+using RestSharp;
+
+namespace RestfulBookerTestFramework.Core
+{
+    public class Client
+    {
+        private readonly RestClient RestClient;
+        private readonly string _baseUrl = ConfigurationProvider.GetTestConfiguration().ApiBaseUrl;
+
+        public Client()
+        {
+            RestClient = new RestClient(_baseUrl);
+        }
+
+        public async Task<RestResponse> ExecuteRequest(RestRequest request)
+        {
+            Logger.LogRequestDetails(request);
+            AllureUtilities.LoginRequestToAllure(request);
+            var response = await RestClient.ExecuteAsync(request);
+            AllureUtilities.LoginResponseToAllure(response);
+            return response;
+        }
+    }
+}
---
+++ restfulbookers/restfulbookers/Configurations/ConfigurationProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestfulBookerTestFramework.Configurations
+{
+    public class ConfigurationProvider
+    {
+        public static TestConfig GetTestConfiguration()
+        {
+            var configuration = new TestConfig();
+            BuildConfiguration().GetSection(nameof(TestConfig)).Bind(configuration);
+            return configuration;
+        }
+
+        private static IConfiguration BuildConfiguration()
+        {
+            // Ładowanie konfiguracji z pliku appsettings.json
+            var _configurationBuilder = new ConfigurationBuilder();
+            return _configurationBuilder
+                .SetBasePath(AppContext.BaseDirectory)  // Ścieżka bazowa (do katalogu aplikacji)
+                .AddJsonFile("Configurations/appconfig.json", optional: false, reloadOnChange: true)  // Ładowanie pliku JSON
+                .Build();
+        }
+    }
+}
